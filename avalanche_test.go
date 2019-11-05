00f8c4616c25f25a4f757c15dd9151e7f2a1803c
奶爸 <hi@nai.ba>
2019-11-05 13:32:18 +0800

    Fix golint

--- avalanche_test.go
+++ avalanche_test.go
@@ -12,16 +12,16 @@ func TestDo(t *testing.T) {
 	var wg sync.WaitGroup
 	for j := 0; j < 200; j++ {
 		wg.Add(1)
-		go func(runnerID int) {
+		go func(runnerID int, tt *testing.T) {
 			defer wg.Done()
 			t.Log("Runner", runnerID, "want to do something")
 			res, err := Do("test", something(t))
 			if err != nil || res.(int64) > 1 {
-				t.FailNow()
+				tt.FailNow()
 				return
 			}
 			t.Log("Runner", runnerID, "getted res", res, err)
-		}(j)
+		}(j, t)
 	}
 	wg.Wait()
 }
